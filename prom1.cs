72df2b0f53f725891b2b830f8a71fa9cf97d6954
ori <ori_larusa@hotmail.com>
2022-10-13 10:42:56 -0300

    agregue el archivo prom1.css para practicar un ejercicio por consola

---
+++ prom1.cs
@@ -0,0 +1,25 @@
+// using System.Collections;
+// namespace prom1
+// {
+// 	class Program
+// 	{
+// 		public static void Main(string[] args)
+// 		{
+// 			string? st;
+// 			ArrayList arrayList=new ArrayList();
+		
+// 			Console.WriteLine("Ingrese Apellido y Documento");
+// 			st=	Console.ReadLine();
+// 			while(st!=""){
+// 				arrayList.Add(st);
+// 				st=	Console.ReadLine();
+// 			}
+//             Console.WriteLine("//////////////////////");
+//             Console.WriteLine("ASi se muestra lo que ingreso");
+//             foreach (var item in arrayList)
+//             {
+//                 Console.WriteLine(item);
+//             }
+//         }			
+// }
+// }
